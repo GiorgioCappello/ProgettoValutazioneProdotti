dc10398060a15c521bcea54f6d376cb26f727cf2
GiorgioCappello <giocap1030@gmail.com>
2024-02-18 16:23:42 +0100

    MC

--- Credito/Credito-parte2.py
+++ Credito/Credito-parte2.py
@@ -2,7 +2,7 @@ import numpy as np
 import math as mt
 from scipy.stats import norm
 from scipy import integrate
-from matplotlib import pyplot
+import matplotlib.pyplot as plt
 
 
 ## punto A
@@ -29,30 +29,45 @@ CVA = - lgd*mt.exp(-r*T)*integrate.quad(integranda, t, T)[0]
 
 print ("Prezzo CVA:", CVA)
 
-'''
-#DA RIVEDERE!!
+## Parte 2
 
 N = 100000
-dt = T/N
+K = 1000
 
-#simulazione dei tempi di fallimento
-default_times = np.random.uniform(0, T, N)    #RAMDOMICO !
+# Simulazione dei tempi di fallimento
+U = np.random.uniform(0,1,N)
+tau = -1/lambda1 * np.log(1-U)
 
-#calcolo del CVA per ciascuna realizzazione
-CVA_simulazioni = []
-for i in range(N):
-    default_time = default_times[i]
-    default_indicator = default_time <= T
-    EAD_i = EAD * default_indicator
-    PD_i = norm.cdf((np.log(X_0 / K) + (r - 0.5 * sigma**2) * T) / (sigma * np.sqrt(T)))
-    CVA_i = calcolo_CVA(-LGD, PD_i, EAD_i, r, T)
-    CVA_simulazioni.append(CVA_i)
+# Istogramma
+plt.hist(tau, bins=K)
+plt.xlabel('Tempi di default')
+plt.ylabel('Numbero di volte osservato')
+plt.title('Tempi di default')
+plt.show()
 
+# Calcolo della probabilità empirica dei tempi di default nei singoli intervalli
+counts, _ = np.histogram(tau, bins=K)
+
+# Calcolo delle probabilità relative
+prob = counts / N
+
+# Funzione per simulazione del sottostante
+def sottostante(X0, sigma, t, z):
+    return X0 + sigma*mt.sqrt(t)*z
+
+# Prezzo sottostante
+I = []
+for i in range(1,N):
+    z = np.random.normal()
+    t = (tau[i-1] + tau[i])/2
+    X = sottostante(X0, sigma, t, z)
+    I.append(prezzo_bc(X, K, sigma, t)*np.exp(-r*t))
+
+
+'''
 #calcolo dell'intervallo di confidenza al 98% per il CVA
 lower_bound = np.percentile(CVA_simulazioni, 1)
 upper_bound = np.percentile(CVA_simulazioni, 99)
 
 print("L'intervallo di confidenza al 98% per il CVA:", (lower_bound, upper_bound))
-
-
 '''
